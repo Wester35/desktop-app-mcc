1f921fafa51d0d93b5ed835ca3d80046a2c3cbb3
Wester35 <40541070+Wester35@users.noreply.github.com>
2025-09-05 14:45:59 +0300

    матрица корелляций

---
+++ analytics/corel_matrix.py
@@ -0,0 +1,42 @@
+from typing import List
+import pandas as pd
+from sqlalchemy.orm import Session
+from models.data_models import MCKData, AnalysisResult
+
+
+def get_data(db: Session, years: List[int]):
+    data_records = db.query(MCKData).filter(MCKData.year.in_(years)).order_by(MCKData.year).all()
+    integral_records = (
+        db.query(AnalysisResult).filter(AnalysisResult.year.in_(years)).order_by(AnalysisResult.year).all())
+
+    integral_dict = {rec.year: rec.integrated_index for rec in integral_records}
+
+    raw_data = []
+    for rec in data_records:
+        raw_data.append({
+            'year': rec.year,
+            'failures_1': rec.failures_1,
+            'failures_2': rec.failures_2,
+            'failures_3': rec.failures_3,
+            'train_losses': rec.train_losses,
+            'investments': rec.investments,
+            'passengers_daily': rec.passengers_daily,
+            'tech_failures': rec.tech_failures,
+            'fare_cost': rec.fare_cost,
+            'interval': rec.interval,
+            'integrated_index': integral_dict.get(rec.year)  # Добавляем интегральный показатель
+        })
+
+    return raw_data
+
+def get_correl_matrix(db: Session, years: List[int]):
+    data = get_data(db, years)
+    df = pd.DataFrame(data)
+    correlation_matrix = df.corr().round(15)
+    return correlation_matrix
+
+pd.set_option('display.max_rows', None)  # Показать все строки
+pd.set_option('display.max_columns', None)  # Показать все столбцы
+pd.set_option('display.width', None)  # Без ограничения ширины
+pd.set_option('display.max_colwidth', None)  # Без ограничения ширины столбцов
+pd.set_option('display.float_format', '{:.4f}'.format)
