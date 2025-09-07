a146e467f918e8e89da6f0a49009f054d619733a
Wester35 <shum0302@gmail.com>
2025-09-07 21:20:05 +0300

    Update equations.py

--- analytics/equations.py
+++ analytics/equations.py
@@ -4,9 +4,9 @@ from analytics.corel_matrix import get_correl_matrix
 from libs.database import get_db
 
 
-def get_indicators(correl_matrix: pd.DataFrame):
+def get_integral_indicators(correl_matrix: pd.DataFrame):
     integral_correl = correl_matrix['integrated_index'].round(15)
-    filtered = integral_correl[abs(integral_correl) > 0.19]
+    filtered = integral_correl[(abs(integral_correl) > 0.19) & (integral_correl.index != 'integrated_index')]
     return filtered
 
 
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', None)
-    pd.set_option('display.max_colwidth', None) 
+    pd.set_option('display.max_colwidth', None)
     pd.set_option('display.float_format', '{:.15f}'.format)
     db = next(get_db())
     from controllers.data_crud import get_all_data
     data = get_all_data(db)
     years = [record.year for record in data]
 
-    print(get_indicators(get_correl_matrix(db, years)))
+    print(get_integral_indicators(get_correl_matrix(db, years)))
