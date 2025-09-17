a73b6a81420b74e221466451488de612ae31c5c2
Wester35 <shum0302@gmail.com>
2025-09-17 23:54:23 +0300

    Update predict.py

--- analytics/predict.py
+++ analytics/predict.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from analytics.reg import build_interval_model
+from analytics.equations import build_interval_model, build_integral_model
 from analytics.prokofiev import predict_prokofiev
 from libs.database import get_db
 
@@ -30,6 +30,32 @@ def calculate_final_predict(db: Session):
 
     return final_prediction
 
+def calculate_final_predict_integral(db: Session):
+    from controllers.data_crud import get_all_data
+    data = get_all_data(db)
+    years = [record.year for record in data]
+    interval_model = build_integral_model(db, years)
+
+    equation_names = [key for key in interval_model['equation'].keys() if key != 'const']
+
+    res_name = []
+    for name in equation_names:
+        result = predict_prokofiev(db, str(name))
+        res_name.append(result)
+
+
+    final_prediction = interval_model['equation']['const']
+
+    for i, name in enumerate(equation_names):
+        coefficient = interval_model['equation'][name]
+        prediction = res_name[i]
+        final_prediction += coefficient * prediction
+
+    print(f"Final prediction: {final_prediction:.6f}")
+
+    return final_prediction
+
 if __name__ == "__main__":
     db = next(get_db())
     calculate_final_predict(db)
+    calculate_final_predict_2(db)
